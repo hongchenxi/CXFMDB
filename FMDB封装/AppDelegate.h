8b646ee0ad42243b1e3a8e36cbc502e0265562ec
hongchenxi <hongchenxi86@gmail.com>
2015-12-28 12:35:15 +0800

    Initial Commit

---
+++ FMDB封装/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDB封装
+//
+//  Created by 洪晨希 on 15/12/28.
+//  Copyright © 2015年 洪晨希. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDB封装/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDB封装
+//
+//  Created by 洪晨希 on 15/12/28.
+//  Copyright © 2015年 洪晨希. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
